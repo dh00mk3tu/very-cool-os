8f2bb79353a668db1a1223a408565e1647415db7
dh00mk3tu <cyberhybird@hotmail.com>
2021-04-09 15:40:33 +0530

    9/4/21 update

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,48 +1,26 @@
-; receiving the data in 'dx'
-; For the examples we'll assume that we're called with dx=0x1234
-print_hex:
+[bits 32] ; using 32-bit protected mode
+
+; this is how constants are defined
+VIDEO_MEMORY equ 0xb8000
+WHITE_ON_BLACK equ 0x0f ; the color byte for each character
+
+print_string_pm:
     pusha
+    mov edx, VIDEO_MEMORY
 
-    mov cx, 0 ; our index variable
+print_string_pm_loop:
+    mov al, [ebx] ; [ebx] is the address of our character
+    mov ah, WHITE_ON_BLACK
 
-; Strategy: get the last char of 'dx', then convert to ASCII
-; Numeric ASCII values: '0' (ASCII 0x30) to '9' (0x39), so just add 0x30 to byte N.
-; For alphabetic characters A-F: 'A' (ASCII 0x41) to 'F' (0x46) we'll add 0x40
-; Then, move the ASCII byte to the correct position on the resulting string
-hex_loop:
-    cmp cx, 4 ; loop 4 times
-    je end
-    
-    ; 1. convert last char of 'dx' to ascii
-    mov ax, dx ; we will use 'ax' as our working register
-    and ax, 0x000f ; 0x1234 -> 0x0004 by masking first three to zeros
-    add al, 0x30 ; add 0x30 to N to convert it to ASCII "N"
-    cmp al, 0x39 ; if > 9, add extra 8 to represent 'A' to 'F'
-    jle step2
-    add al, 7 ; 'A' is ASCII 65 instead of 58, so 65-58=7
+    cmp al, 0 ; check if end of string
+    je print_string_pm_done
 
-step2:
-    ; 2. get the correct position of the string to place our ASCII char
-    ; bx <- base address + string length - index of char
-    mov bx, HEX_OUT + 5 ; base + length
-    sub bx, cx  ; our index variable
-    mov [bx], al ; copy the ASCII char on 'al' to the position pointed by 'bx'
-    ror dx, 4 ; 0x1234 -> 0x4123 -> 0x3412 -> 0x2341 -> 0x1234
+    mov [edx], ax ; store character + attribute in video memory
+    add ebx, 1 ; next char
+    add edx, 2 ; next video memory position
 
-    ; increment index and loop
-    add cx, 1
-    jmp hex_loop
+    jmp print_string_pm_loop
 
-end:
-    ; prepare the parameter and call the function
-    ; remember that print receives parameters in 'bx'
-    mov bx, HEX_OUT
-    call print
-
+print_string_pm_done:
     popa
     ret
-
-HEX_OUT:
-    db '0x0000',0 ; reserve memory for our new string
-
-    
