4ba91d5802790c7902a1de47c995d7223cca8ea9
dh00mk3tu <cyberhybird@hotmail.com>
2021-04-21 14:54:33 +0530

    This commit is made by Auto-Git. Commit-40, Wednesday 21 April 2021 02:13:24 PM IST

--- resources/install-depnd.sh
+++ resources/install-depnd.sh
@@ -1,16 +1,16 @@
 echo Installing Dependencies 
 
-sudo apt install gcc
-sudo apt install gcc
-sudo apt install make
-sudo apt install bison
-sudo apt install flex
-sudo apt install libgmp3-dev
-sudo apt install libgmpc-dev
-sudo apt install libgmpfr-dev
-sudo apt install texinfo
-sudo apt install libisl-dev
-sudo apt install libcloog-isl-dev
+sudo apt install gcc -y
+sudo apt install gcc -y
+sudo apt install make -y
+sudo apt install bison -y
+sudo apt install flex -y
+sudo apt install libgmp3-dev -y
+sudo apt install libgmpc-dev -y
+sudo apt install libgmpfr-dev -y
+sudo apt install texinfo -y
+sudo apt install libisl-dev -y
+sudo apt install libcloog-isl-dev -y
 
 echo Done
 
