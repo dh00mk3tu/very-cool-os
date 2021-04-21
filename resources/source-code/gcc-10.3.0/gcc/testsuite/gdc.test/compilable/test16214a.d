4d21a3d18c9f0d2913abfedb460e88f6261f7401
Anirudh Rath <38985076+dh00mk3tu@users.noreply.github.com>
2021-04-21 17:12:52 +0530

    ok

--- resources/source-code/gcc-10.3.0/gcc/testsuite/gdc.test/compilable/test16214a.d
+++
@@ -1,7 +0,0 @@
-// EXTRA_SOURCES: imports/test16214b.d
-// REQUIRED_ARGS: -Icompilable/imports
-
-module test16214a;
-import test16214b;
-
-class LibasyncEventLoop : EventLoop {}
--- resources/source-code/gcc-10.3.0/gcc/testsuite/gdc.test/fail_compilation/fail19890a.d
+++
@@ -1,7 +0,0 @@
-// PERMUTE_ARGS:
-/*
----
-fail_compilation/fail19890a.d(8): Error: `void[/^[0-9]+(LU)?$/]` size 1 * /^[0-9]+$/ exceeds 0x7fffffff size limit for static array
----
-*/
-void[] f = cast(void[-1]) "a";
